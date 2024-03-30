a4cf8ab334785ad02477dfc007e0e4305ea2732f
Shubham <shubhamverma286008@gmail.com>
2024-03-30 10:02:57 +0530

    updated download.js

--- mess/menu/download.js
+++ mess/menu/download.js
@@ -5,7 +5,7 @@ function generatePDF() {
         .set({
             margin: 1,
             filename: "mess-menu.pdf",
-            image: { type: "jpeg", quality: 0.98 },
+            // image: { type: "jpeg", quality: 0.98 },
             html2canvas: { scale: 2 },
             jsPDF: { unit: "in", format: "letter", orientation: "landscape" }
         })
